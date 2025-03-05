f9cafd81f6a1f22d4af96f1f0cb13875d2305295
Gabriel Dias <gdias@fastprobr.com>
2025-03-04 23:32:33 -0300

    feat: add favicon and meta title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/assets/favicon.png" />
+        <title>Gami Studio</title>
+      </head>
       <LanguageProvider>
         <LoadingProvider>
           <body className={`${gabarito.className}`}>
